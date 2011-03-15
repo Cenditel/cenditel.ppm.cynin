339803f61dcd180714efd2034e8c8a67225f9cbf
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2011-03-15 23:52:53 +0000

    Definir un nuevo vocabulario movido de content/project aqui y definir que los tipos de contenidos son agregables desde este producto

--- cenditel/ppm/cynin/config.py
+++ cenditel/ppm/cynin/config.py
@@ -1,11 +1,22 @@
 """Common configuration constants
 """
 
+from Products.Archetypes.utils import DisplayList
+
+from cenditel.ppm import ppmMessageFactory as _
+
 PROJECTNAME = 'cenditel.ppm.cynin'
 
 ADD_PERMISSIONS = {
-    # -*- extra stuff goes here -*-
-    'proposals': 'cenditel.ppm: Add proposals',
+    'folderproj': 'cenditel.ppm.cynin: Add folderproj',
+    'proposals': 'cenditel.ppm.cynin: Add proposals',
     'project': 'cenditel.ppm.cynin: Add project',
-    'folderproj': 'cenditel.ppm: Add folderproj',
 }
+
+TYPE_SUBFOLDER_PROJECT = DisplayList((
+    ("Events", _(u"Events/Activities")),
+    ("Folder", _(u"Folder")),
+    ("Ploneboard", _(u"Discussions board")),
+    ("PoiTracker", _(u"Demands")),
+    ("Weblog", _(u"Blog")),
+    ))
